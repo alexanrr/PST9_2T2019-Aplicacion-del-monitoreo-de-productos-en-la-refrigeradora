9989265eda710a267109c0820b7c1804854485d6
jesfacap <52768497+jesfacap@users.noreply.github.com>
2020-01-26 20:30:43 -0500

    Add files via upload
    archivo donde se encuentra el código de arduino, el cual tara las celdas de carga dando un valor casi cercano al valor exacto al producto colocado en las celdas de carga, y este a la vez envía esos datos al código de Firebase_Nodemcu

--- Arduino/PESOS.ino
+++ Arduino/PESOS.ino
@@ -6,82 +6,81 @@
 #define DT  13
 #define SCK  12
 
-#define DT1 11
-#define SCK1 10
+#define DT1 31 //pin pwm 31
+#define SCK1 30 //pin pwm 10
 
-#define DT2 9
-#define SCK2 8
+#define DT2 29  //pin pwm 9
+#define SCK2 28  //pin pwm 8
 
-#define DT3 7
-#define SCK3 6
+#define DT3 27  //pint pwm 7
+#define SCK3 26 //pin pwm 6
 
 // Creacion del objeto para el transmisor de celda de carga HX711
 HX711 balanza(DT, SCK);
 HX711 balanza1(DT1, SCK1);
-HX711 balanza2(DT2, SCK2);
 HX711 balanza3(DT3, SCK3);
+HX711 balanza2(DT2, SCK2);
 
 
-// escala para tarar o encerar la celda con un peso inicial
-int ESCALA = 114000 / 312;
+// escala
+float ESCALA = 114000 / 301.5;
 
-int bal;
-int bal1;
-int bal2;
-int bal3;
 
-//comunicacion serial
+float bal;
+float bal1;
+float bal2;
+float bal3;
+
 #include <SoftwareSerial.h>
 SoftwareSerial s(4, 5); //Rx,Tx
+
 void setup() {
   Serial.begin(9600);
   s.begin(9600);
-  
-  
-  Serial.print("Celda 1             ");
-  Serial.print("Celda 2           ");
-  Serial.print("Celda 3          ");
-  Serial.println("Celda 4");
-//setear la escala al momento de encerar
-  balanza.set_scale(ESCALA); // Establecemos la ESCALA calculada anteriormente
+  Serial.print("     Agua             ");
+  Serial.print("Lacteos              ");
+  Serial.print("Jugo             ");
+  Serial.println("Gaseosa");
+
+  balanza2.set_scale(ESCALA); // Establecemos la ESCALA calculada anteriormente
   balanza1.set_scale(ESCALA); //Establecemos la ESCALA calculada anterior
-  balanza2.set_scale(ESCALA);
   balanza3.set_scale(ESCALA);
-  
-  balanza.tare(20); // El peso actual es considerado Tara.
+  balanza.set_scale(ESCALA);
+
+  balanza2.tare(20); // El peso actual es considerado Tara.
   balanza1.tare(20); //El peso actual es considerado tara (el soporte)
-  balanza2.tare(20);
   balanza3.tare(20);
-  
-  delay(5000); //Esperar 5 segundo para comenzar a pesar 
+  balanza.tare(20);
+
+  delay(5000); //Esperar 5 segundo para comenzar a pesar
 
 }
 void loop() {
-  
- //muestra los pesos en el terminal
+
+  //muestra por pantalla los valores de porcentaje de 23,5 cm real = 20 cm en arduino y los pesoso obtenidos
   Serial.print("Peso: ");
   Serial.print(balanza.get_units(20), 0);
   Serial.print("g          ");
-  Serial.print("Peso:  ");
+  Serial.print("Peso: ");
   Serial.print(balanza1.get_units(20), 0);
   Serial.print("g          ");
-  Serial.print("Peso:  ");
+  Serial.print("Peso: ");
   Serial.print(balanza2.get_units(20), 0);
   Serial.print("g          ");
-  Serial.print("Peso:  ");
+  Serial.print("Peso: ");
   Serial.print(balanza3.get_units(20), 0); // Se obtiene el valor real del peso en Kg del elemento
   Serial.println("g");
 
   //obtener valores de cada balanza en gramos
-  bal = abs(balanza.get_units(20));
-  bal1 = abs(balanza1.get_units(20));
-  bal2 = abs(balanza2.get_units(20));
-  bal3 = abs(balanza3.get_units(20));
-  
+  bal = balanza.get_units(20);
+  bal1 = balanza1.get_units(20);
+  bal2 = balanza2.get_units(20);
+  bal3 = balanza3.get_units(20);
+
   //pasando informacion al nodemcu
-  s.write(bal/8.0);
-  s.write(bal1/8.0);
-  s.write(bal2/8.0);
-  s.write(bal3/8.0);
+  s.println(bal);
+  s.println(bal1);
+  s.println(bal2);
+  s.println(bal3);
   delay(1000);
 }
